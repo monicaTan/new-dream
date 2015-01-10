920b5d18e9a4f987c63f3dc16b155242281c8268
monica <tangerinetan@126.com>
2015-01-10 14:56:15 +0800

    change

--- js/navmouse.js
+++ js/navmouse.js
@@ -1,9 +1,12 @@
 $(function(){
 	function navMouse(){
 		$(".nav-ul li a").mouseover(function(){
-			var navW = $(this).find('span').width();
-			var emW = Math.floor(navW);
-			$(this).find('em').css({"display":"inline-block","min-width":emW+"px","_width":emW+"px"});
+			var spanW = $(this).find('span').width(),
+			    spanFW = Math.floor(spanW),
+				emW = $(this).find('em').width(),
+				emFW = Math.floor(emW);
+
+			$(this).find('em').css({"display":"inline-block","min-width":spanFW+"px","_width":spanFW+"px"});
 			$(this).find('span').hide().next('em').show();
 		});
 		$(".nav-ul li a").mouseout(function(){
